778a1e62dcbcbc25d069b9dad55d95786182afc2
Andreas Lappe <a.lappe@zimmer7.com>
2016-11-21 19:01:48 +0100

    Remove useless code…

--- Classes/Utility/HttpUtility.php
+++ Classes/Utility/HttpUtility.php
@@ -25,9 +25,6 @@ class HttpUtility {
 		$ch = curl_init();
 		curl_setopt($ch, CURLOPT_URL, $url);
 		curl_setopt($ch, CURLOPT_CUSTOMREQUEST, $verb);
-		if ($payload !== NULL) {
-			curl_setopt($ch, CURLOPT_POSTFIELDS, $payload);
-		}
 		curl_setopt($ch, CURLOPT_RETURNTRANSFER, TRUE);
 		curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
 
