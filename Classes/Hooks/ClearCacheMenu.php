abc65fed23a3c64df2518a8ef120386cb9951bb0
Philip Majewski <pille72@gmail.com>
2019-04-02 17:01:50 +0200

    Adjust AJAX url fetching in BE

--- Classes/Hooks/ClearCacheMenu.php
+++ Classes/Hooks/ClearCacheMenu.php
@@ -1,6 +1,8 @@
 <?php
 namespace Z7\Varnish\Hooks;
+use TYPO3\CMS\Backend\Routing\UriBuilder;
 use TYPO3\CMS\Backend\Utility\BackendUtility;
+use TYPO3\CMS\Core\Utility\GeneralUtility;
 
 class ClearCacheMenu implements \TYPO3\CMS\Backend\Toolbar\ClearCacheActionsHookInterface
 {
@@ -15,11 +17,12 @@ class ClearCacheMenu implements \TYPO3\CMS\Backend\Toolbar\ClearCacheActionsHook
     public function manipulateCacheActions(&$cacheActions, &$optionValues)
     {
         if ($GLOBALS['BE_USER']->isAdmin()) {
+            $uriBuilder = GeneralUtility::makeInstance(UriBuilder::class);
             $cacheActions[] = [
                 'id' => 'varnish',
                 'title' => 'LLL:EXT:varnish/Resources/Private/Language/locallang.xlf:hooks.cache.title',
                 'description' => 'LLL:EXT:varnish/Resources/Private/Language/locallang.xlf:hooks.cache.description',
-                'href' => BackendUtility::getAjaxUrl('varnish::banAll'),
+                'href' => (string) $uriBuilder->buildUriFromRoute('ajax_varnish::banAll'),
                 'iconIdentifier' => 'actions-ext-varnish-clear'
             ];
             $optionValues[] = 'banAll';
--- Configuration/Backend/AjaxRoutes.php
+++ Configuration/Backend/AjaxRoutes.php
@@ -2,6 +2,6 @@
 return [
     'varnish::banAll' => [
         'path' => '/varnish/banAll',
-        'target' => Z7\Varnish\Hooks\Ajax::class . '->banAll'
+        'target' => Z7\Varnish\Hooks\Ajax::class . '::banAll'
     ],
 ];
