79bf702532bbbf96fd8c48c57a05c3622cd853ef
Philip Majewski <pille72@gmail.com>
2019-04-02 17:23:38 +0200

    Fix ajax controller

--- Classes/Hooks/Ajax.php
+++ Classes/Hooks/Ajax.php
@@ -1,14 +1,20 @@
 <?php
 namespace Z7\Varnish\Hooks;
+use TYPO3\CMS\Core\Http\JsonResponse;
 use Z7\Varnish\Controller\VarnishController;
 
 class Ajax
 {
 
-    public function banAll()
+    /**
+     * @param ServerRequestInterface $request
+     * @return JsonResponse
+     */
+    public function banAll($request)
     {
         $logger = \TYPO3\CMS\Core\Utility\GeneralUtility::makeInstance('TYPO3\CMS\Core\Log\LogManager')->getLogger(__CLASS__);
         $logger->info('User ' . $GLOBALS['BE_USER']->user['username'] . ' has cleared the varnish cache');
         VarnishController::ban('banAll');
+        return new JsonResponse(['success' => true]);
     }
 }
